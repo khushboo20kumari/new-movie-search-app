474bdfc4e224f47d78fec623ecb12db40cb3e5bc
khushboo24kumari <khushboo.kumari@navgurukul.org>
2024-02-28 15:20:04 +0530

    fetch data from api

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,26 @@
 import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function Main() {
-    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=89eef3426d167c3c8145a257ebe68357&%27").then((res)=>{
-        return res.json()        
-    }).then(res=>{
-        
-    })
+
+    const [moviesData, setMoviesData] = useState([])
+
+    const fetchData = () => {
+
+        fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=89eef3426d167c3c8145a257ebe68357&%27").then((res) => {
+            return res.json()
+
+        }).then((res) => {
+            setMoviesData(res)
+        })
+    }
+    
+    useEffect(() => {
+        fetchData()
+    },[])
+
+    console.log(moviesData, "data")
+
     return (
         <Box style={{ backgroundColor: "#373b69" }}>
 
